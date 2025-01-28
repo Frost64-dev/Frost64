3f16ed6af09be3deef3c5e1ae673046d46eae41a
Frosty515 <frosty515yt@gmail.com>
2025-01-28 20:44:58 +1000

    Fixed the C++20 format test
    The test in the previous commit turned out to not work. This has now
    been fixed.

--- build-scripts/test-cxx20-format.cpp
+++
@@ -1,9 +0,0 @@
-#include <format>
-#include <iostream>
-#include <string>
-
-int main(int argc, char**) {
-    std::string str = std::format("This is a test, argc = {}", argc);
-    std::cout << str;
-    return 0;
-}
